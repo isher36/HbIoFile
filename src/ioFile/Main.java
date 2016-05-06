9ca72ff91600a8784d54822ce0fd94df0eea41de
jonas Bardin <isher36@gmail.com>
2016-05-06 16:56:31 +0200

    add surchage for input text and createDirectory

--- src/ioFile/Main.java
+++ src/ioFile/Main.java
@@ -2,6 +2,7 @@ package ioFile;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Scanner;
@@ -12,18 +13,40 @@ public class Main {
 
 	public static void main(String[] args) {
 		// ask user a path
-		String path = inputText("Veuillez saisir le chemin d'un point d'entrée d'une arborescence");
+		String pathText = inputText("Veuillez saisir le chemin d'un point d'entrée d'une arborescence");
+		Path path = Paths.get(pathText);
 		// if path doesn't exist we create it
 		createDirectory(path);
 
-		boolean addTowDirectory = inputTextYesOrNo("Voulez vous créer une arborescence à 2 niveau? yes , no",new String[] {"yes","no"});
+		boolean addTowDirectory = inputTextYesOrNo("Voulez vous créer une arborescence à 2 niveau? yes , no");
+		if ( addTowDirectory ){
+			//create first directory
+			createDirectory(path,"rep1");
+			//create second directory
+			createDirectory(path,"rep2");
+		}
+		else
+		{
+			System.out.println("ok on crée rien!");
+		}
 	}
 
-	public static boolean createDirectory(String path) {
-		if (Files.notExists(Paths.get(path))) {
+	// create directory with sub folder
+	public static boolean createDirectory(Path path,String SubDirectory) {
+		Path uri = path.resolve(path);
+		// if we have a sub directory, we add it to path
+		if ( SubDirectory != null)
+			uri = uri.resolve(SubDirectory);
+		return createDirectory(uri);
+	}
+	
+	// create directory
+	public static boolean createDirectory(Path path) {
+			
+		if (Files.notExists(path)) {
 			try {
-				Files.createDirectory(Paths.get(path));
-				System.out.println(path + " non trouvé, ce chemin a été créé.");
+				Files.createDirectory(path);
+				System.out.println(path.toString() + " non trouvé, ce chemin a été créé.");
 				return true;
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
@@ -31,7 +54,7 @@ public class Main {
 				return false;
 			}
 		} else {
-			System.out.println(path + " existant et trouvé");
+			System.out.println(path.toString() + " existant et trouvé");
 			return false;
 		}
 	}
@@ -43,14 +66,14 @@ public class Main {
 		return str;
 	}
 
+	// ask yes or no to user and return true or false
 	public static boolean inputTextYesOrNo(String text ) {
 		String returntext ; 
 		String[] param = new String[] { "yes", "no"};
 		do {returntext = inputText(text); }
 		while (! Arrays.asList(param).contains(returntext));
 			
-		//returntext
-		return true;
+		return returntext.equals("yes") ? true : false ;
 	}
 
 }
